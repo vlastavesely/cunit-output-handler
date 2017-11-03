af5c2458b4c390233fc7ba99a21105a06b4b746f
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-11-03 10:04:13 +0100

    update test application

--- test.c
+++ test.c
@@ -1,27 +1,42 @@
 #include <CUnit/CUnit.h>
 #include "cunit-minimal.h"
 
-static void test_1(void)
+static void test_passing(void)
 {
 	CU_ASSERT_EQUAL(11, 11);
+	usleep(10 * 1000);
 }
 
-static void test_1_failing(void)
+static void test_failing(void)
 {
 	CU_ASSERT_EQUAL(11, 1);
 }
 
 static void cunit_add_tests(struct CU_Suite *suite)
 {
-	CU_add_test(suite, "test_1", test_1);
-	CU_add_test(suite, "test_2", test_1);
-	CU_add_test(suite, "test_3", test_1);
-	CU_add_test(suite, "test_4", test_1);
-	CU_add_test(suite, "test_5", test_1);
-	CU_add_test(suite, "test_6", test_1);
-	CU_add_test(suite, "test_7", test_1_failing);
-	CU_add_test(suite, "test_8", test_1);
-	CU_add_test(suite, "test_9", test_1);
+	CU_add_test(suite, "lorem_ipsum_dolor_sit_amet", test_passing);
+	CU_add_test(suite, "consectetur_adipiscing_elit", test_passing);
+	CU_add_test(suite, "phasellus_ullamcorper_nulla_consequat", test_passing);
+	CU_add_test(suite, "euismod_congue", test_passing);
+	CU_add_test(suite, "quisque_tincidunt_pellentesque_arcu_odio_augue", test_passing);
+	CU_add_test(suite, "dapibus_sit_amet_justo", test_passing);
+	CU_add_test(suite, "pretium_rhoncus_neque", test_passing);
+	CU_add_test(suite, "vivamus_consectetur_felis_odio", test_passing);
+	CU_add_test(suite, "vitae_congue_urna_tincidunt_a", test_passing);
+	CU_add_test(suite, "nullam_semper_mollis_libero_laoreet_iaculis", test_passing);
+	CU_add_test(suite, "purus_sodales_eget", test_passing);
+	CU_add_test(suite, "curabitur_non_tortor_eu_diam_viverra_eleifend", test_passing);
+	CU_add_test(suite, "etiam_pulvinar_purus_nunc", test_passing);
+	CU_add_test(suite, "a_molestie_ante_semper_tempor", test_failing);
+	CU_add_test(suite, "nam_porttitor_ipsum_id_ultricies_finibus", test_passing);
+	CU_add_test(suite, "donec_nulla eros", test_passing);
+	CU_add_test(suite, "gravida_id_maximus_pellentesque", test_passing);
+	CU_add_test(suite, "malesuada_id_risus", test_passing);
+	CU_add_test(suite, "ut_finibus_ligula_est_et_blandit_elit_molestie_eget", test_passing);
+	CU_add_test(suite, "etiam_suscipit_ipsum_sagittis", test_failing);
+	CU_add_test(suite, "quam_scelerisque", test_passing);
+	CU_add_test(suite, "at_ornare", test_passing);
+	CU_add_test(suite, "tortor_pharetra_cras_non_elit_erat", test_passing);
 }
 
 int main(int argc, const char **argv)
